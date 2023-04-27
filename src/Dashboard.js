62cf719b76ad7b685f0aeac1a18c38780d15f55f
inesgarridolo <115874418+inesgarridolo@users.noreply.github.com>
2023-04-27 13:29:53 +0200

    Update Dashboard.js

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -4,13 +4,44 @@ import ListaMisHerramientas from "./ListaMisHerramientas"
 import { useEffect, useState } from "react";
 
 export default function Dashboard(props) {
+    const [puntuacionMedia, setPuntuacionMedia] = useState(0);
+    const [estrellas, setEstrellas] = useState(0);
 
     let herramientas = props.tools.filter(product => product.userId == 1);
+    
+    useEffect(() => {
+        const puntuacionMediaUser = () => {
+            let puntuacionesFiltradasUserId = props.puntuaciones.filter(usuario => usuario.userId == props.users[0].id);
+            console.log(puntuacionesFiltradasUserId);
+            let puntuacionTotal = 0;
+            let nValoraciones = 0;
+            let puntuacionMedia = 0;
+            puntuacionesFiltradasUserId.map(puntuaciones => {
+                puntuacionTotal +=  puntuaciones.puntuacion;
+                nValoraciones++;
+            })
+            puntuacionMedia = Math.floor(puntuacionTotal/nValoraciones);
+            return puntuacionMedia;
+            
+        }
+        setPuntuacionMedia(puntuacionMediaUser());
+    }, [props.users[0].id, props.puntuaciones]);
+
+    useEffect(() =>{
+        const estrellasMedia = () => {
+            const rutaEstrellas = `/${puntuacionMedia}estrellas.png`;
+            return rutaEstrellas;
+        }
+        setEstrellas(estrellasMedia());
+    });
+    
     return (<div id="dashboard">
         <div className="dashboardZona1">
             <img className="profilePhoto" src={props.users[0].fotoUser} width="250px" height="250px"></img>
             <div id="dashboardDatos">
                 <h2>{props.users[0].nombre}</h2>
+                <img src={estrellas}></img>
+                <p><b>Puntuación: {puntuacionMedia}</b></p>
                 <Link to={'/edituser/1'}><button className="loginEnter">Editar datos</button></Link>
             </div>
         </div>
