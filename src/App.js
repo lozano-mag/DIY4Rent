a7554d30d859ea289eb4e8e4ad716ac3e1598e43
inesgarridolo <115874418+inesgarridolo@users.noreply.github.com>
2023-04-27 13:20:23 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,12 +13,14 @@ import EditUser from './EditUser';
 import Dashboard from './Dashboard';
 import SubirHerramienta from './SubirHerramienta';
 import EditarHerramientas from './EditarHerramientas';
+import Valoracion from './Valoracion';
 
 function App() {
 
   const [herramientas, setHerramientas] = useState([]);
   const [usuarios, setUsuarios] = useState([]);
   const [carga, setCarga]=useState(true);
+  const [puntuaciones, setPuntuaciones] = useState(0);
 
   useEffect(() => {
     async function fetchData(){
@@ -28,6 +30,9 @@ function App() {
       await fetch('/api/usuarios')
       .then(response => response.json())
       .then(data => setUsuarios(data));
+      await fetch('/api/puntuaciones')
+      .then(response => response.json())
+      .then(data => setPuntuaciones(data));
       setTimeout(() => {
         setCarga(false);
       }, 500);
@@ -41,15 +46,17 @@ function App() {
       {carga ? <img id='loading' className='spinner' src='spinner.gif'/> : 
       <Routes>
         <Route path={"/"} element={<Principal tools={herramientas} users={usuarios}/>}/>
-        <Route path={"/herramientas/:productId"} element={<Herramientas users={usuarios} tools={herramientas}/>}/>
+        <Route path={"/herramientas/:productId"} element={<Herramientas users={usuarios} tools={herramientas} puntuaciones={puntuaciones}/>}/>
         <Route path={"/login"} element={<Login/>}/>
         <Route path={"/register"} element={<Register/>}/>
         <Route path={'/usuarios'} element={<Usuarios users={usuarios}/>} />
-        <Route path={'/usuarios/:userId'} element={<UserPage users={usuarios} tools={herramientas}/>}/>
+        <Route path={'/usuarios/:userId'} element={<UserPage users={usuarios} tools={herramientas} puntuaciones={puntuaciones}/>}/>
         <Route path={'/edituser/:userId'} element={<EditUser users={usuarios}/>}/>
-        <Route path={'/dashboard'}  element={<Dashboard users={usuarios} tools={herramientas}/>}/>
+        <Route path={'/dashboard'}  element={<Dashboard users={usuarios} tools={herramientas} puntuaciones={puntuaciones}/>}/>
         <Route path={'/herramienta_upload'} element={<SubirHerramienta/>}/>
         <Route path={'/herramienta_edit/:productId'} element={<EditarHerramientas tools={herramientas}/>}/>
+        <Route path={'/valoracion/:userId'} element={<Valoracion users={usuarios} puntuaciones={puntuaciones}/>}/>
+
       </Routes>
       }
     </div>
