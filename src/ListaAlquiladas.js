06830b289450ec23fb71c26f51f9c258a0422114
inesgarridolo <115874418+inesgarridolo@users.noreply.github.com>
2023-04-27 13:36:57 +0200

    Update ListaAlquiladas.js

--- src/ListaAlquiladas.js
+++ src/ListaAlquiladas.js
@@ -1,5 +1,8 @@
+import { Link } from "react-router-dom"
+
 export default function ListaAlquiladas(props) {
     return(<div>
         <p>Nada por aquí ¡Comienza a alquilar!</p>
+        <Link to={`/valoracion/${0}`}><button>Finalizar</button></Link> 
     </div>)
 }
